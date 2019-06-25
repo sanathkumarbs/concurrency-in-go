a5fc6f32ed9f8f3cd08d21171427398d2e8dea19
Sanath Kumar <b.s.sanathkumar@gmail.com>
2019-06-25 15:02:19 -0700

    Pool in sync

--- patterns/5_pool/pool.go
+++ patterns/5_pool/pool.go
@@ -12,6 +12,30 @@
 
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
+	myPool := sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Creating new instance.")
+			return struct{}{}
+		},
+	}
+
+	// No object in Pool, creates a new one
+	instance1 := myPool.Get()
+
+	// No object in Pool - which is available, creates a new one
+	instance2 := myPool.Get()
+
+	myPool.Put(instance1)
+
+	// Reuses the pool member instance1
+	myPool.Get()
+
+	myPool.Put(instance2)
 
 }
